7cc3715b86c3179c71245915908c836d35c344c4
alejandro <alejandrozapata@gmail.com>
2023-07-04 15:46:21 -0300

    conexion bd

--- clase4/practico/usuario.php
+++ clase4/practico/usuario.php
@@ -4,6 +4,85 @@
 echo "estructura armada alumnos!!!";
 
 
+var_dump($_GET);
+
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
+
+
+//Conexion
+/*
+//lugar alojado:
+localhost...128.10.10.1
+
+//usuario: xampp
+root
+
+//password:
+""
+
+//nombre de Base de datos:
+empresacac2023 
+
+
+
+*/
+$conexion = mysqli_connect("localhost","root","","empresacac2023");
+
+if(mysqli_connect_errno()){
+    echo "error de conexion";
+}else{
+    echo "funciona todo ok!!!!!";
+}
+
+
+//consultas......
+// mysqli_query();
+
+$consultas = mysqli_query($conexion,"SELECT * FROM alumnos");
+
+
+echo "<pre>";
+var_dump($consultas);
+echo "</pre>";
+
+// $listado = mysqli_fetch_assoc($consultas);
+
+// echo "<pre>";
+// var_dump($listado);
+// echo "</pre>";
+
+// $listadoArray = mysqli_fetch_array($consultas);
+
+// echo "<pre>";
+// var_dump($listadoArray);
+// echo "</pre>";
+
+
+echo "<br>";
+echo "<h2>probando</h2>";
+
+// echo $listadoArray['nombre'];
+
+while( $listadoArray = mysqli_fetch_array($consultas)){
+
+    
+    echo $listadoArray['nombre'] . " " . $listadoArray['apellido'] . " " .$listadoArray['correo'];
+    echo "<br>";
+
+
+}
+
+
+
+
+
+
+
+//errores...
+
+
 
 
 ?>
